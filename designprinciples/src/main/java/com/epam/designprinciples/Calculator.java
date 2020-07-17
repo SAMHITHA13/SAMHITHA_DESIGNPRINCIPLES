7980ccc21af5f8d97143077fffccd7b6824b8bdf
SAMHITHA13 <61277389+SAMHITHA13@users.noreply.github.com>
2020-07-17 17:27:17 +0530

    Add files via upload

---
+++ designprinciples/src/main/java/com/epam/designprinciples/Calculator.java
@@ -0,0 +1,18 @@
+package com.epam.designprinciples;
+
+public class Calculator {
+		
+	double add(double x, double y) {
+			return x + y;
+		}
+		double subtract(double x, double y) {
+			return x - y;
+		}
+		double multiply(double x, double y) {
+			return x * y;
+		}
+		double divide(double x, double y) {
+			return x / y;
+		}
+
+}
